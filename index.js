0f5bab8083bc7e1908dde6195e21737f8c83f15b
priyam <priyamshankar.5@gmail.com>
2022-01-14 16:09:20 +0530

    added the dynamic text button

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 let plus = document.getElementById("plus");
 plus.addEventListener("click", plusfnc);
+let counter = 0;
 function plusfnc() {
-    let txtbox = document.createElement("p");
-    txtbox.id="txid";
-    
-    //    txtbox.id=txid;
-    //    txtbox.setAttribute("type","text");
-    let text = document.createTextNode("This is a paragraph.");
-    txtbox.appendChild(text);
-    let txid=document.getElementById("txid");
-    txid.innerHTML="<br>heyy there
+    counter++;
+    if (counter == 1) {
+        var dlt = document.createElement("button");
+        dlt.id = "dltid";
+        document.body.appendChild(dlt);
+        var dltid = document.getElementById("dltid");
+        dltid.innerHTML = ("minus the texbox");
+    }
+    const txtbox = document.createElement("input");
+    txtbox.className = "txid";
+
+    txtbox.setAttribute("type", "text");
+    document.body.appendChild(txtbox);
+    plus.innerHTML = ("plus more");
+    if (counter == 5) {
+        alert("5 text box created");
+    }
+    // const dltid = document.getElementsByClassName(".dltid");
+    dltid.addEventListener("click", dltfnc);
+    function dltfnc() {
+        const txid = document.querySelector(".txid");
+        txid.remove();
+        // alert("dlt clicked");
+        counter--;
+        if (counter == 0) {
+            document.getElementById("dltid").remove();
+        }
+    }
 }
+document.getElementById("btn2").addEventListener("click", function(){
+    
+})

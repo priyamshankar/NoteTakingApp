b57aef2a82e2083055bf6ae30246db3d07daeb72
priyam <priyamshankar.5@gmail.com>
2022-01-15 00:05:27 +0530

    adding the searching funciton

--- index.js
+++ index.js
@@ -49,8 +49,11 @@ function notesShow() {
         </div>`;
     });
     document.getElementById("oops").innerHTML = domShow;
+    if (strnoteobj==null && strtitleobj==null){
+        domShow.innerHTML=("No notes found!! Add some please");
+    }
 }
-function deleteNote(index){
+function deleteNote(index) {
     // alert(index);
     let strnote = localStorage.getItem("strnote");
     let strTitle = localStorage.getItem("strTitle");
@@ -64,9 +67,30 @@ function deleteNote(index){
         strnoteobj = JSON.parse(strnote);
         strtitleobj = JSON.parse(strTitle);
     }
-    strnoteobj.splice(index,1);
-    strtitleobj.splice(index,1);
-    localStorage.setItem("strnote",JSON.stringify(strnoteobj));
-    localStorage.setItem("strTitle",JSON.stringify(strtitleobj));
+    strnoteobj.splice(index, 1);
+    strtitleobj.splice(index, 1);
+    localStorage.setItem("strnote", JSON.stringify(strnoteobj));
+    localStorage.setItem("strTitle", JSON.stringify(strtitleobj));
     notesShow();
 }
+
+let search = document.getElementById("search");
+search.addEventListener("input", function () {
+    let card = document.getElementsByClassName("card");
+    Array.from(card).forEach(function (element) {
+        let noteTxt = element.getElementsByTagName("p")[0].innerText;
+        if (noteTxt.includes(search.value)) {
+            element.style.display = "inline-flex";
+        }
+        else {
+            element.style.display = "none";
+        }
+        let titlTxt = element.getElementsByTagName("h5")[0].innerText;
+        if (titlTxt.includes(search.value)) {
+            element.style.display = "inline-flex";
+        }
+        else {
+            element.style.display = "none";
+        }
+    });
+})

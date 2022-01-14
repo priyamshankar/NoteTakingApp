a90ba843cf10fab98b43ab81e3f0cbe59623378c
priyam <priyamshankar.5@gmail.com>
2022-01-14 21:31:44 +0530

    note taking feature

---
+++ index.js
@@ -0,0 +1,52 @@
+notesShow();
+let title = document.getElementById("exampleFormControlInput1");
+let note = document.getElementById("exampleFormControlTextarea1");
+let add_note = document.getElementById("add_note");
+add_note.addEventListener("click", function () {
+    let strnoteobj;
+    let strtitleobj;
+    let strnote = localStorage.getItem("strnote");
+    let strTitle = localStorage.getItem("strTitle");
+    if (strnote == null && strTitle == null) {
+        strnoteobj = [];
+        strtitleobj = [];
+    }
+    else {
+        strnoteobj = JSON.parse(strnote);
+        strtitleobj = JSON.parse(strTitle);
+    }
+    strnoteobj.push(note.value);
+    strtitleobj.push(title.value);
+    localStorage.setItem("strnote", JSON.stringify(strnoteobj));
+    localStorage.setItem("strTitle", JSON.stringify(strtitleobj));
+    title.value = ("");
+    note.value = ("");
+    notesShow();
+});
+function notesShow() {
+    let strnote = localStorage.getItem("strnote");
+    let strTitle = localStorage.getItem("strTitle");
+    // let strnoteobj;
+    // let strtitleobj;
+    if (strnote == null && strTitle == null) {
+        strnoteobj = [];
+        strtitleobj = [];
+    }
+    else {
+        strnoteobj = JSON.parse(strnote);
+        strtitleobj = JSON.parse(strTitle);
+    }
+    let domShow = "";
+    strnoteobj.forEach(function (element, index) {
+        domShow += `  <div class="card text-dark bg-warning mb-3 mx-3 my-3" style="max-width: 18rem;">
+        <div class="card-header">Note ${index + 1} <button type="button" class="btn-close" aria-label="Close"
+                style="float: right;" id="deleteNote"></button></div>
+        <div class="card-body">
+            <h5 class="card-title">${strtitleobj[index]}</h5>
+            <p class="card-text">${element}</p>
+            <button type="button" class="btn btn-outline-light" >Light</button>
+        </div>
+        </div>`;
+    });
+    document.getElementById("oops").innerHTML = domShow;
+}
